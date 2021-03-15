a7230235bd54586deaaf3e31619549b965377f18
jacace <javierandrescaceres@gmail.com>
2021-03-15 13:28:47 +0000

    Added sample creation of kinesis with TF

---
+++ 1.kinesis/main.tf
@@ -0,0 +1,13 @@
+resource "aws_kinesis_stream" "test_stream" {
+  name             = "test_stream"
+  shard_count      = 1
+  retention_period = 24
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+
+  #kms_key_id = ""
+  
+}
